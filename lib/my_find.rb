2fc8ce7c3332f4e7382d2a05b9c999f244c1e1bc
psarma89 <priyam.sarma@gmail.com>
2017-11-08 19:26:30 -0500

    Adds find solution

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -1,5 +1,14 @@
 require 'pry'
 
 def my_find(collection)
+  i =0
 
+  return_array = []
+  while i < collection.length
+    if yield(collection[i])
+      return collection[i]
+    else
+      i += 1
+    end
+  end
 end
