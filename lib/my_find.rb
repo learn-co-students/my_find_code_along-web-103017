ddfa32704e6b63e591e78723b0ea9c9fb46941ea
RochelLevi <rochlevi@gmail.com>
2017-11-08 19:56:37 -0500

    Done.

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -2,4 +2,13 @@ require 'pry'
 
 def my_find(collection)
 
+  i = 0
+  bool = false
+  while i < collection.length && bool == false
+    bool = yield(collection[i])
+    i += 1
+  end
+
+  bool == true ? collection[i - 1] : nil
+
 end
